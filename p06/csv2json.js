724a7a6faae72a54ab98ee2c3bc69c6b6df81784
Schazad Mohammad <mohamsch@students.zhaw.ch>
2022-10-28 00:16:19 +0200

    p06 continued

--- p06/csv2json.js
+++ p06/csv2json.js
@@ -2,25 +2,34 @@ const fs = require('fs');
 const { readFile } = require('fs').promises
 
 function readCSV(filepath) {
-    readFile(filepath, 'utf8').then(data => csv2json(data))
+    return readFile(filepath, 'utf8')
 }
 
 function csv2json(csv) {
-    let lineSeparetadCSVArr = csv.split('\n')
-    let header = lineSeparetadCSVArr[0].split(',')
-    let jsonArr = []
-
-    for (let i = 1; i < lineSeparetadCSVArr.length; i++) {
-        let tmpLine = lineSeparetadCSVArr[i].split(',')
-        let tmpObj = {}
-        for (let j = 0; j < tmpLine.length; j++) {
-            tmpObj[header[j]] = tmpLine[j]
+    return new Promise((resolve, reject) => {
+        try {
+            let lineSeparetadCSVArr = csv.split('\n')
+            let header = lineSeparetadCSVArr[0].split(',')
+            let jsonArr = []
+            for (let i = 1; i < lineSeparetadCSVArr.length; i++) {
+                let tmpLine = lineSeparetadCSVArr[i].split(',')
+                let tmpObj = {}
+                for (let j = 0; j < tmpLine.length; j++) {
+                    tmpObj[header[j]] = tmpLine[j]
+                }
+                jsonArr.push(tmpObj)
+            }
+            resolve(JSON.stringify(jsonArr))
+        } catch (err) {
+            reject(err)
         }
-        jsonArr.push(tmpObj)
-    }
+    })
+}
 
-    let json = JSON.stringify(jsonArr)
-    console.log(json);
+function saveFile(filepath, content) {
+    return fs.writeFile(filepath, content, err => {
+        if (err) console.error(err)
+    });
 }
 
 function logStatsOfFile(filepath) {
@@ -54,5 +63,18 @@ function logStatsOfFile(filepath) {
 
 // logStatsOfFile('csv/population.csv')
 readCSV('csv/population_test.csv')
+    .catch(() => {
+        console.log("Error reading file")
+    })
+    .then(data => csv2json(data))
+    .catch(() => {
+        console.log("Error parsing csv -> json")
+    })
+    .then(data => saveFile('json/data.json', data))
+    .catch(() => {
+        console.log("Error writing json file")
+    })
+
+
 // get arguments of console
 // process.argv[2]

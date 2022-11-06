e95dd0199eed5a4ab99d7457886a7b4071d70f4f
Schazad Mohammad <mohamsch@students.zhaw.ch>
2022-11-06 02:30:36 +0100

    p08 done

---
+++ p08/script.js
@@ -0,0 +1,53 @@
+const n_rows = 6
+const n_columns = 7
+let state = Array(n_rows).fill('').map(el => Array(n_columns).fill(''))
+
+function elt(type, attrs, ...children) {
+    let node = document.createElement(type)
+    for (a in attrs) {
+        node.setAttribute(a, attrs[a])
+    }
+    for (let child of children) {
+        if (typeof child != "string")
+            node.appendChild(child)
+        else
+            node.appendChild(document.createTextNode(child))
+    }
+    return node
+}
+
+function showBoard() {
+    let board = document.getElementsByClassName('board')[0]
+    board.innerHTML = ''
+    for (let i = 0; i < state.length; i++) {
+        let row = elt('div', { 'class': 'row' })
+        for (let j = 0; j < state[i].length; j++) {
+            let piece = undefined
+            switch (state[i][j]) {
+                case 'r':
+                    piece = elt('div', { 'class': 'piece red' })
+                    break
+                case 'b':
+                    piece = elt('div', { 'class': 'piece blue' })
+                    break
+            }
+            let field = elt('div', { 'class': 'field' }, (piece) ? piece : '')
+            row.appendChild(field)
+        }
+        board.appendChild(row)
+    }
+}
+
+function randomPicker() {
+    setInterval(() => {
+        let options = ['', 'r', 'b']
+        let row = Math.floor(Math.random() * n_rows)
+        let column = Math.floor(Math.random() * n_columns)
+        state[row][column] = options[Math.floor(Math.random() * options.length)]
+        showBoard()
+    }, 1000)
+}
+
+function init() {
+    randomPicker()
+}
